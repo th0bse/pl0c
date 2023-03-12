bf721d6d66a4814ec5ed1aa24e39c620be8f0dc3
Thorben von Rekowski <thorben.von-rekowski@gwdg.de>
2023-03-12 15:55:18 +0100

    refactor: use simpler SyntaxTree in the parser

--- src/main/kotlin/com/th0bse/pl0c/parser/Parser.kt
+++ src/main/kotlin/com/th0bse/pl0c/parser/Parser.kt
@@ -16,154 +16,225 @@ class Parser {
     }
 
     private fun program() {
-        val root = Leaf(SyntaxTreeNode(NonTerminal.PROGRAM))
+        val root = SyntaxTreeNode(NonTerminal.PROGRAM)
         syntaxTree = SyntaxTree(root)
         block(root)
         if (stream.peek() == Symbol.PERIOD)
-            root.leaves.add(Leaf(SyntaxTreeNode(Terminal.STOP, stream.next())))
-        else throw RuntimeException("Dot (.) expected")
+            root.children.add(SyntaxTreeNode(Terminal.STOP, stream.next()))
+        else throw RuntimeException("Dot \".\" expected")
     }
 
-    private fun block(leaf: Leaf) {
-        var blockLeaf = Leaf(SyntaxTreeNode(NonTerminal.BLOCK))
-        var currentLeaf: Leaf
-        leaf.leaves.add(blockLeaf)
+    private fun block(node: SyntaxTreeNode) {
+        var blockNode = SyntaxTreeNode(NonTerminal.BLOCK)
+        var currentNode: SyntaxTreeNode
+        node.children.add(blockNode)
         if (stream.peek() == Keyword.CONST) {
             do {
                 stream.next() // discard const keyword / separating comma
-                currentLeaf = Leaf(SyntaxTreeNode(NonTerminal.CONSTANT))
-                blockLeaf.leaves.add(currentLeaf)
-                constant(currentLeaf)
+                currentNode = SyntaxTreeNode(NonTerminal.CONSTANT)
+                blockNode.children.add(currentNode)
+                constant(currentNode)
             } while (stream.peek() == Symbol.COMMA)
-            if (stream.next() != Symbol.SEMICOLON) throw RuntimeException("Semicolon (;) expected")
+            if (stream.next() != Symbol.SEMICOLON) throw RuntimeException("Semicolon \";\" expected")
         }
         if (stream.peek() == Keyword.VAR) {
             do {
                 stream.next() // discard var keyword / separating comma
-                currentLeaf = Leaf(SyntaxTreeNode(NonTerminal.VARIABLE))
-                blockLeaf.leaves.add(currentLeaf)
-                variable(currentLeaf)
+                currentNode = SyntaxTreeNode(NonTerminal.VARIABLE)
+                blockNode.children.add(currentNode)
+                variable(currentNode)
             } while (stream.peek() == Symbol.COMMA)
-            if (stream.next() != Symbol.SEMICOLON) throw RuntimeException("Semicolon (;) expected")
+            if (stream.next() != Symbol.SEMICOLON) throw RuntimeException("Semicolon \";\" expected")
         }
         if (stream.peek() == Keyword.PROCEDURE) {
             while (stream.peek() == Keyword.PROCEDURE) {
-                procedure(blockLeaf)
+                procedure(blockNode)
             }
         }
 
         println("DEBUG")
 
-        statement(blockLeaf)
+        statement(blockNode)
     }
 
-    private fun constant(leaf: Leaf) {
+    private fun constant(node: SyntaxTreeNode) {
         if (stream.peek() is Identifier) {
-            leaf.leaves.add(Leaf(SyntaxTreeNode(Terminal.IDENTIFIER, stream.next())))
-            if (stream.peek() == Symbol.EQUALS) {
-                leaf.leaves.add(Leaf(SyntaxTreeNode(Terminal.EQUAL, stream.next())))
+            node.children.add(SyntaxTreeNode(Terminal.IDENTIFIER, stream.next()))
+            if (stream.peek() == Symbol.ASSIGNMENT) {
+                node.children.add(SyntaxTreeNode(Terminal.EQUAL, stream.next()))
                 if (stream.peek() is Num) {
-                    leaf.leaves.add(Leaf(SyntaxTreeNode(Terminal.NUMBER, stream.next())))
+                    node.children.add(SyntaxTreeNode(Terminal.NUMBER, stream.next()))
                 } else throw RuntimeException("Number expected")
-            } else throw RuntimeException("Equality sign (=) expected")
+            } else throw RuntimeException("Assignment operator \":=\" expected")
         } else throw RuntimeException("Identifier expected")
     }
 
-    private fun variable(leaf: Leaf) {
+    private fun variable(node: SyntaxTreeNode) {
         if (stream.peek() is Identifier) {
-            leaf.leaves.add(Leaf(SyntaxTreeNode(Terminal.IDENTIFIER, stream.next())))
+            node.children.add(SyntaxTreeNode(Terminal.IDENTIFIER, stream.next()))
         } else throw RuntimeException("Identifier expected")
     }
 
-    private fun procedure(leaf: Leaf) {
-        var currentLeaf = Leaf(SyntaxTreeNode(Terminal.PROCEDURE, stream.next()))
-        leaf.leaves.add(currentLeaf)
+    private fun procedure(node: SyntaxTreeNode) {
+        var currentNode = SyntaxTreeNode(NonTerminal.PROCEDURE, stream.next())
+        node.children.add(currentNode)
 
         if (stream.peek() is Identifier) {
-            currentLeaf.leaves.add(Leaf(SyntaxTreeNode(Terminal.IDENTIFIER, stream.next())))
-            if (stream.peek() != Symbol.SEMICOLON) throw RuntimeException("Semicolon (;) expected")
-            block(currentLeaf)
+            currentNode.children.add(SyntaxTreeNode(Terminal.IDENTIFIER, stream.next()))
+            if (stream.next() != Symbol.SEMICOLON) throw RuntimeException("Semicolon \";\" expected")
+            if (stream.next() != Keyword.BEGIN) throw RuntimeException("Begin keyword \"begin\" expected")
+            if (stream.next() != Symbol.SEMICOLON) throw RuntimeException("Semicolon \";\" expected")
+
+            block(currentNode)
         }
+
+        if (stream.next() != Symbol.SEMICOLON) throw RuntimeException("Semicolon \";\" expected")
+        if (stream.next() != Keyword.END) // discard end keyword
+            throw RuntimeException("End keyword \"end\" expected")
+        if (stream.next() != Symbol.SEMICOLON) // discard semicolon token
+            throw RuntimeException("Semicolon \";\" expected")
     }
 
-    private fun statement(leaf: Leaf) {
-        var currentLeaf = Leaf(SyntaxTreeNode(NonTerminal.STATEMENT))
-        leaf.leaves.add(currentLeaf)
+    private fun statement(node: SyntaxTreeNode) {
+        var currentNode = node
         when (stream.peek()) {
             is Identifier -> {
-                currentLeaf.leaves.add(Leaf(SyntaxTreeNode(Terminal.IDENTIFIER, stream.next())))
-                if (stream.peek() == Symbol.ASSIGNMENT) {
-                    currentLeaf.leaves.add(Leaf(SyntaxTreeNode(Terminal.ASSIGNMENT, stream.next())))
-                    expression(currentLeaf)
-                }
+                assignment(currentNode)
             }
 
             Keyword.CALL -> {
-                currentLeaf.leaves.add(Leaf(SyntaxTreeNode(Terminal.CALL, stream.next())))
+                currentNode.children.add(SyntaxTreeNode(Terminal.CALL, stream.next()))
                 if (stream.peek() is Identifier) {
-                    currentLeaf.leaves.add(Leaf(SyntaxTreeNode(Terminal.IDENTIFIER, stream.next())))
+                    currentNode.children.add(SyntaxTreeNode(Terminal.IDENTIFIER, stream.next()))
                 }
             }
 
             Keyword.BEGIN -> {
                 stream.next() // discard begin keyword, or semicolon token
+                statement(currentNode)
                 while (stream.next() == Symbol.SEMICOLON) {
-                    statement(currentLeaf)
+                    if (stream.peek() == Keyword.END) continue
+                    statement(currentNode)
                 }
-                if (stream.next() != Keyword.END) throw RuntimeException("End expected")
             }
 
-            // add remaining cases: IF, WHILE
+            Keyword.IF -> {
+                stream.next() // discard if keyword
+                var conditionNode = SyntaxTreeNode(NonTerminal.CONDITION)
+                node.children.add(conditionNode)
+                condition(conditionNode)
+                if (stream.next() != Keyword.THEN) throw RuntimeException("Keyword \"then\" expected")
+                statement(conditionNode)
+            }
+
+            Keyword.WHILE -> {
+                stream.next() // discard while keyword
+                var loopNode = SyntaxTreeNode(NonTerminal.WHILE)
+                node.children.add(loopNode)
+                condition(loopNode)
+                if (stream.next() != Keyword.DO) throw RuntimeException("Keyword \"do\" expected")
+                statement(loopNode)
+            }
+
+            Keyword.END -> {
+                stream.next()
+                if (stream.next() != Symbol.PERIOD) throw RuntimeException("Dot \".\" expected")
+            }
 
             else -> throw RuntimeException("Not a valid statement")
         }
     }
 
-    private fun expression(leaf: Leaf) {
-        var currentLeaf = Leaf(SyntaxTreeNode(NonTerminal.EXPRESSION))
-        leaf.leaves.add(currentLeaf)
-        term(currentLeaf)
+    private fun assignment(node: SyntaxTreeNode) {
+        var currentNode = SyntaxTreeNode(NonTerminal.ASSIGNMENT)
+        node.children.add(currentNode)
+        currentNode.children.add(SyntaxTreeNode(Terminal.IDENTIFIER, stream.next()))
+        if (stream.next() == Symbol.ASSIGNMENT) { // discard assignment operator
+            expression(currentNode)
+        }
+    }
+
+    private fun condition(node: SyntaxTreeNode) {
+        if (stream.peek() == Keyword.ODD) {
+            node.children.add(SyntaxTreeNode(Terminal.ODD, stream.next()))
+        } else {
+            expression(node)
+            when (stream.peek()) {
+                Symbol.EQUALS -> {
+                    node.children.add((SyntaxTreeNode(Terminal.EQUAL, stream.next())))
+                }
+
+                Symbol.NOT_EQUALS -> {
+                    node.children.add(SyntaxTreeNode(Terminal.DIFFERENT, stream.next()))
+                }
+
+                Symbol.LESS_THAN -> {
+                    node.children.add(SyntaxTreeNode(Terminal.LESS, stream.next()))
+                }
+
+                Symbol.GREATER_THAN -> {
+                    node.children.add(SyntaxTreeNode(Terminal.GREATER, stream.next()))
+                }
+
+                Symbol.LESS_EQUAL -> {
+                    node.children.add(SyntaxTreeNode(Terminal.LESS_EQUAL, stream.next()))
+                }
+
+                Symbol.GREATER_EQUAL -> {
+                    node.children.add(SyntaxTreeNode(Terminal.GREATER_EQUAL, stream.next()))
+                }
+            }
+            expression(node)
+        }
+    }
+
+    private fun expression(node: SyntaxTreeNode) {
+        var currentNode = SyntaxTreeNode(NonTerminal.EXPRESSION)
+        node.children.add(currentNode)
+        if (stream.peek() != Symbol.MINUS) { // we don't need a term in case of unary minus
+            term(currentNode)
+        }
         while (stream.peek() == Symbol.PLUS || stream.peek() == Symbol.MINUS) {
             if (stream.peek() == Symbol.PLUS) {
-                currentLeaf.leaves.add(Leaf(SyntaxTreeNode(Terminal.PLUS, stream.next())))
+                currentNode.children.add(SyntaxTreeNode(Terminal.PLUS, stream.next()))
             } else {
-                currentLeaf.leaves.add(Leaf(SyntaxTreeNode(Terminal.MINUS, stream.next())))
+                currentNode.children.add(SyntaxTreeNode(Terminal.MINUS, stream.next()))
             }
-            term(currentLeaf)
+            term(currentNode)
         }
     }
 
-    private fun term(leaf: Leaf) {
-        var currentLeaf = Leaf(SyntaxTreeNode(NonTerminal.TERM))
-        leaf.leaves.add(currentLeaf)
-        factor(currentLeaf)
+    private fun term(node: SyntaxTreeNode) {
+        var currentNode = SyntaxTreeNode(NonTerminal.TERM)
+        node.children.add(currentNode)
+        factor(currentNode)
         while (stream.peek() == Symbol.MULTIPLY || stream.peek() == Symbol.DIVIDE) {
             if (stream.peek() == Symbol.MULTIPLY) {
-                currentLeaf.leaves.add(Leaf(SyntaxTreeNode(Terminal.MULTIPLY, stream.next())))
+                currentNode.children.add(SyntaxTreeNode(Terminal.MULTIPLY, stream.next()))
             } else {
-                currentLeaf.leaves.add(Leaf(SyntaxTreeNode(Terminal.DIVIDE, stream.next())))
+                currentNode.children.add(SyntaxTreeNode(Terminal.DIVIDE, stream.next()))
             }
-            factor(currentLeaf)
+            factor(currentNode)
         }
     }
 
-    private fun factor(leaf: Leaf) {
-        var currentLeaf = Leaf(SyntaxTreeNode(NonTerminal.FACTOR))
-        leaf.leaves.add(currentLeaf)
+    private fun factor(node: SyntaxTreeNode) {
+        var currentNode = SyntaxTreeNode(NonTerminal.FACTOR)
+        node.children.add(currentNode)
         when (stream.peek()) {
             is Identifier -> {
-                currentLeaf.leaves.add(Leaf(SyntaxTreeNode(Terminal.IDENTIFIER, stream.next())))
+                currentNode.children.add(SyntaxTreeNode(Terminal.IDENTIFIER, stream.next()))
             }
 
             is Num -> {
-                currentLeaf.leaves.add(Leaf(SyntaxTreeNode(Terminal.NUMBER, stream.next())))
+                currentNode.children.add(SyntaxTreeNode(Terminal.NUMBER, stream.next()))
             }
 
             Symbol.LPAREN -> {
                 stream.next() // discard left parenthesis, association is already represented in the tree
-                expression(currentLeaf)
+                expression(currentNode)
                 if (stream.next() != Symbol.RPAREN)
-                    throw RuntimeException("Right parenthesis ()) expected.")
+                    throw RuntimeException("Right parenthesis \")\" expected.")
             }
         }
     }
