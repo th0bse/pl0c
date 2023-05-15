8fb6c184f6924fee354ceefa6284c33a0d82c8a9
Thorben von Rekowski <thorben.von-rekowski@gwdg.de>
2023-05-15 15:11:26 +0200

    chore: add printTree function
    The printTree function aids in debugging by pringing the syntax tree
    generated by the parser.

--- src/main/kotlin/com/th0bse/pl0c/Pl0c.kt
+++ src/main/kotlin/com/th0bse/pl0c/Pl0c.kt
@@ -3,6 +3,7 @@ package com.th0bse.pl0c
 import com.th0bse.pl0c.generator.CodeGenerator
 import com.th0bse.pl0c.lexer.Lexer
 import com.th0bse.pl0c.parser.Parser
+import com.th0bse.pl0c.parser.SyntaxTreeNode
 import com.th0bse.pl0c.semantics.SemanticAnalyzer
 import com.th0bse.pl0c.vm.PL0VM
 import java.io.BufferedReader
@@ -10,23 +11,32 @@ import java.io.InputStreamReader
 
 class Pl0c {
 
-    private val lexer = Lexer()
-    private val parser = Parser()
-    private val semanticAnalyzer = SemanticAnalyzer()
-    private val generator = CodeGenerator()
+  private val lexer = Lexer()
+  private val parser = Parser()
+  private val semanticAnalyzer = SemanticAnalyzer()
+  private val generator = CodeGenerator()
 
-    fun read() {
-        var tokens = lexer.readUntilEndOrError(BufferedReader(InputStreamReader(System.`in`)))
-        var tree = parser.parse(tokens)
-        tree = semanticAnalyzer.analyze(tree)
-        var program = generator.generateProgram(tree)
-        PL0VM().execute(program)
-    }
+  fun read() {
+    var tokens = lexer.readUntilEndOrError(BufferedReader(InputStreamReader(System.`in`)))
+    var tree = parser.parse(tokens)
+    printTree(tree.root, 0)
+    tree = semanticAnalyzer.analyze(tree)
+    var program = generator.generateProgram(tree)
+    PL0VM().execute(program)
+  }
 
-    companion object {
-        @JvmStatic
-        fun main(args: Array<String>) {
-            Pl0c().read()
-        }
+  fun printTree(node: SyntaxTreeNode, level: Int) {
+    for (i in 0 until level) {
+      print("  ")
     }
+    println(node)
+    node.children.forEach { printTree(it, level + 1) }
+  }
+
+  companion object {
+    @JvmStatic
+    fun main(args: Array<String>) {
+      Pl0c().read()
+    }
+  }
 }
